42aa9a4627e875d356a180edbfb71e6984899710
Aniket <aniket.g21@gmail.com>
2016-09-11 15:55:01 +0800

    start and stop capture

--- resources/js/preview.js
+++ resources/js/preview.js
@@ -6,9 +6,14 @@ function reload_img () {
 function error_img () {
   setTimeout("mjpeg_img.src = 'cam_pic.php?time=' + new Date().getTime();", 100);
 }
-function init() {
+function initCapture() {
   mjpeg_img = document.getElementById("mjpeg_dest");
-  //mjpeg_img.onload = reload_img;
-  //mjpeg_img.onerror = error_img;
+
+  mjpeg_img.onload = reload_img;
+  mjpeg_img.onerror = error_img;
   reload_img();
+}
+function stopCapture() {
+	mjpeg_img.onload = function(){};
+  mjpeg_img.onerror = function(){}
 }
