58fb0946b7e0d80e1eb44513b98d54fa3ba6719d
aniket <aniket.g21@gmail.com>
2016-09-12 05:33:03 +0000

    use setInterval instead of onload

--- resources/js/preview.js
+++ resources/js/preview.js
@@ -1,19 +1,27 @@
 var mjpeg_img;
+var flag;
  
 function reload_img () {
   mjpeg_img.src = "preview?time=" + new Date().getTime() + "&pDelay=100000";
 }
+
 function error_img () {
   setTimeout("mjpeg_img.src = 'preview?time=' + new Date().getTime();", 100);
 }
+
 function initCapture() {
   mjpeg_img = document.getElementById("mjpeg_dest");
+  flag = true;
+  //mjpeg_img.onload = reload_img;
+  //mjpeg_img.onerror = error_img;
 
-  mjpeg_img.onload = reload_img;
-  mjpeg_img.onerror = error_img;
-  reload_img();
+  var reloadID = setInterval(function(){
+     if(!flag) clearInterval(reloadID); 
+     reload_img();
+  }, 250);
 }
 function stopCapture() {
-  mjpeg_img.onload = function(){};
-  mjpeg_img.onerror = function(){}
+  //mjpeg_img.onload = function(){};
+  //mjpeg_img.onerror = function(){};
+  flag = false;
 }
